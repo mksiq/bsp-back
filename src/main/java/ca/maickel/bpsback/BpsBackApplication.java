bc7c78bb30c30a3956a58996351f3dab9e710998
mksiq <maickelsiqueira@gmail.com>
2020-12-23 08:28:46 -0500

    spring project start

---
+++ src/main/java/ca/maickel/bpsback/BpsBackApplication.java
@@ -0,0 +1,13 @@
+package ca.maickel.bpsback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BpsBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BpsBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ca/maickel/bpsback/BpsBackApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.maickel.bpsback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BpsBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

5967ab6036d13e5abfda1610d0a63b1af748010a
mksiq <msiqueira@myseneca.ca>
2020-12-23 10:08:48 -0500

    added base for domains

---
+++ src/main/java/ca/maickel/bpsback/domain/Photo.java
@@ -0,0 +1,4 @@
+package ca.maickel.bpsback.domain;
+
+public class Photo {
+}
---
+++ src/main/java/ca/maickel/bpsback/domain/Tag.java
@@ -0,0 +1,60 @@
+package ca.maickel.bpsback.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+import java.util.Objects;
+
+
+@Entity
+public class Tag implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String tag;
+
+    public Tag() {
+    }
+
+    public Tag(Integer id, String tag) {
+        this.id = id;
+        this.tag = tag;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Tag tag1 = (Tag) o;
+
+        if (!Objects.equals(id, tag1.id)) return false;
+        return Objects.equals(tag, tag1.tag);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (tag != null ? tag.hashCode() : 0);
+        return result;
+    }
+}
